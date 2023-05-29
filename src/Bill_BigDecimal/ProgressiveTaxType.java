97525135fb1ca4f3e49f87f4e94ef277d6f9e4e7
Damir Kadyrzhanov <damirk120404@gmail.com>
2023-05-29 11:26:42 +0600

    switch to if

--- src/Bill_BigDecimal/ProgressiveTaxType.java
+++ src/Bill_BigDecimal/ProgressiveTaxType.java
@@ -3,12 +3,11 @@ package Bill_BigDecimal;
 import java.math.BigDecimal;
 
 public class ProgressiveTaxType extends TaxType {
-
     @Override
     public BigDecimal calculateTaxFor(BigDecimal amount) {
-        switch (amount.compareTo(BigDecimal.valueOf(100000))){
-            case 1: return amount.multiply(BigDecimal.valueOf(0.15));
-            default: return amount.multiply(BigDecimal.valueOf(0.1));
+        if (amount.compareTo(BigDecimal.valueOf(100000)) >= 0) {
+            return amount.multiply(BigDecimal.valueOf(0.15));
         }
+        return amount.multiply(BigDecimal.valueOf(0.1));
     }
 }
